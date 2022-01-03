ffe37551321e831c443427bc7cdaaaa6250fc9f4
Edede Oiwoh <eoiwoh@gmail.com>
2022-01-03 01:23:32 -0500

    feat: Clean up types

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,12 @@
 /**
  * An Object which should be validated
  */
-export type BaseTarget = {[key: string]: any};
+export type BaseTarget = {[key: string]: unknown};
 
 /**
  * A function which tests if a given parameter is valid.
  */
-export type ValidationFunction<T = any> = (arg: T) => Boolean;
+export type ValidationFunction<T = unknown> = (arg: T) => boolean;
 
 /**
  * A collection of validation functions.
