a93bb51bce7ee61835cdcd67f52778fbe9743e7e
Edede Oiwoh <eoiwoh@gmail.com>
2023-03-20 12:50:30 -0400

    feat: better type safety,  nullish and pattern validators

--- src/tests/validators.test.ts
+++ src/tests/validators.test.ts
@@ -2,11 +2,11 @@ import * as Validators from '../validators';
 
 describe('Validators', () => {
   describe('array', () => {
-    test('Can validate an array type', () => {
+    test('validates array types', () => {
       expect(Validators.array([])).toBe(true);
     });
 
-    test('Fails non-array types', () => {
+    test('fails non-array types', () => {
       expect(Validators.array('array')).toBe(false);
       expect(Validators.array(1)).toBe(false);
       expect(Validators.array({})).toBe(false);
@@ -14,11 +14,11 @@ describe('Validators', () => {
   });
 
   describe('number', () => {
-    test('Can validate an number type', () => {
+    test('can validate an number type', () => {
       expect(Validators.number(1)).toBe(true);
     });
 
-    test('Fails non-number types', () => {
+    test('fails non-number types', () => {
       expect(Validators.number('number')).toBe(false);
       expect(Validators.number([])).toBe(false);
       expect(Validators.number({})).toBe(false);
@@ -26,11 +26,11 @@ describe('Validators', () => {
   });
 
   describe('object', () => {
-    test('Can validate an object type', () => {
+    test('can validate an object type', () => {
       expect(Validators.object({})).toBe(true);
     });
 
-    test('Fails non-object types', () => {
+    test('fails non-object types', () => {
       expect(Validators.object([])).toBe(false);
       expect(Validators.object('object')).toBe(false);
       expect(
@@ -42,11 +42,11 @@ describe('Validators', () => {
   });
 
   describe('string', () => {
-    test('Can validate an string type', () => {
+    test('can validate an string type', () => {
       expect(Validators.string('string')).toBe(true);
     });
 
-    test('Fails non-string types', () => {
+    test('fails non-string types', () => {
       expect(Validators.string({})).toBe(false);
       expect(Validators.string([])).toBe(false);
       expect(
@@ -58,11 +58,11 @@ describe('Validators', () => {
   });
 
   describe('boolean', () => {
-    test('Can validate an boolean type', () => {
+    test('can validate an boolean type', () => {
       expect(Validators.boolean(true)).toBe(true);
     });
 
-    test('Fails non-boolean types', () => {
+    test('fails non-boolean types', () => {
       expect(Validators.boolean({})).toBe(false);
       expect(Validators.boolean([])).toBe(false);
       expect(
@@ -74,12 +74,12 @@ describe('Validators', () => {
   });
 
   describe('optional', () => {
-    test('Can validate an optional type', () => {
+    test('validates optional types when present', () => {
       expect(Validators.optional(Validators.string)('string')).toBe(true);
       expect(Validators.optional(Validators.string)(undefined)).toBe(true);
     });
 
-    test('Fails non-optional types', () => {
+    test('fails if provided type does not match the required', () => {
       expect(Validators.optional(Validators.string)({})).toBe(false);
       expect(Validators.optional(Validators.string)([])).toBe(false);
       expect(
@@ -91,20 +91,47 @@ describe('Validators', () => {
   });
 
   describe('combine', () => {
-    test('Can validate an combined type', () => {
-      expect(
-        Validators.combine(Validators.string, Validators.number)('string')
-      ).toBe(false);
-      expect(Validators.combine(Validators.string, Validators.number)(1)).toBe(
-        false
+    test('can validate combinations', () => {
+      const validateNumAsString = Validators.combine(
+        Validators.string,
+        Validators.number
+      );
+      const validateDateAsNum = Validators.combine(
+        Validators.number,
+        Validators.date
       );
 
+      expect(validateNumAsString('string')).toBe(false);
+      expect(validateNumAsString(1)).toBe(false);
+      expect(validateDateAsNum(10)).toBe(true);
+    });
+
+    test('can validate a combination with optional types', () => {
       expect(
-        Validators.combine(Validators.number, Validators.integer)(10)
+        Validators.combine(
+          Validators.optional(Validators.string),
+          Validators.optional(Validators.number)
+        )('string')
+      ).toBe(false);
+      expect(
+        Validators.combine(
+          Validators.optional(Validators.string),
+          Validators.optional(Validators.number)
+        )(undefined)
       ).toBe(true);
     });
 
-    test('Fails non-combined types', () => {
+    test('can validate provided and custom types', () => {
+      const validator = Validators.combine(
+        Validators.pattern(/[a-z]+@gmail.com$/i),
+        (value) => `${value}`.length > 10
+      );
+
+      expect(validator('string')).toBe(false);
+      expect(validator('example@gmail.com')).toBe(true);
+    });
+
+    test('fails non-combined types', () => {
       expect(Validators.combine(Validators.string, Validators.number)({})).toBe(
         false
       );
@@ -122,14 +149,61 @@ describe('Validators', () => {
     });
   });
 
-  describe('integer', () => {
-    test('Can validate an integer type', () => {
-      expect(Validators.integer(1)).toBe(true);
+  describe('arrayOf', () => {
+    test('can validate an array of type', () => {
+      expect(Validators.arrayOf(Validators.string)(['string'])).toBe(true);
     });
 
-    test('Fails non-integer types', () => {
-      expect(Validators.integer(1.1)).toBe(false);
-      expect(Validators.integer('1')).toBe(false);
+    test('fails non-array of types', () => {
+      expect(Validators.arrayOf(Validators.string)(['string', 1])).toBe(false);
+      expect(Validators.arrayOf(Validators.string)([1])).toBe(false);
+    });
+  });
+
+  describe('nullish', () => {
+    test('can validate an nullish type', () => {
+      expect(Validators.nullish(null)).toBe(true);
+      expect(Validators.nullish(undefined)).toBe(true);
+    });
+
+    test('fails non-nullish types', () => {
+      expect(Validators.nullish(1)).toBe(false);
+      expect(Validators.nullish('string')).toBe(false);
+      expect(Validators.nullish({})).toBe(false);
+    });
+  });
+
+  describe('pattern', () => {
+    test('can validate a pattern', () => {
+      expect(Validators.pattern(/^string$/i)('string')).toBe(true);
+    });
+
+    test('fails if pattern input is not string', () => {
+      expect(Validators.pattern(/^string$/i)(1)).toBe(false);
+    });
+
+    test('fails if pattern does not match', () => {
+      expect(Validators.pattern(/^string$/i)('string1')).toBe(false);
+    });
+  });
+
+  describe('date', () => {
+    test('validates various input types for dates', () => {
+      expect(Validators.date(1679326882357)).toBe(true);
+      expect(Validators.date('2020-01-01')).toBe(true);
+      expect(Validators.date(new Date())).toBe(true);
+      expect(Validators.date(1.1)).toBe(true);
+    });
+
+    test('fails non valid inputs', () => {
+      expect(Validators.date('string')).toBe(false);
+      expect(Validators.date({})).toBe(false);
+      expect(Validators.date([])).toBe(false);
+      expect(
+        Validators.date(() => {
+          /**/
+        })
+      ).toBe(false);
     });
   });
 });
--- src/validators.ts
+++ src/validators.ts
@@ -1,34 +1,50 @@
-type Validator = (arg: unknown) => boolean;
-type ValidatorOp = (...validatorFn: Validator[]) => Validator;
-
 /**
  * Ensure a given argument is an array using `Array.isArray`.
  */
-export const array: Validator = (arg) => Array.isArray(arg);
+export function array<T = unknown>(arg: unknown): arg is T[] {
+  return Array.isArray(arg);
+}
+
+/**
+ * Ensure a given argument is an array of a given type.
+ * This is useful for validating arrays of objects.
+ * @param validatorFn A validator function to apply to each element.
+ */
+export function arrayOf<T>(validatorFn: TypedValidator<T>) {
+  return (arg: unknown): arg is T[] => {
+    if (!array(arg)) {
+      return false;
+    }
+
+    return arg.every(validatorFn);
+  };
+}
 
 /**
  * Ensure a given argument is a boolean using `typeof`.
  */
-export const boolean: Validator = (arg) => typeof arg === 'boolean';
+export function boolean(arg: unknown): arg is boolean {
+  return typeof arg === 'boolean';
+}
 
 /**
- * Combine multiple validators into a single result.
+ * Ensure a given argument is a date using the underlying `Date` object.
  */
-export const combine: ValidatorOp =
-  (...validators) =>
-  (arg) =>
-    validators.every((validator) => validator(arg));
-
-/**
- * Ensure a given argument is an integer using `typeof` and `Number.isInteger`.
- */
-export const integer: Validator = (arg) =>
-  typeof arg === 'number' && Number.isInteger(arg);
+export function date(arg: unknown): arg is Date {
+  if (string(arg)) {
+    return !isNaN(Date.parse(arg));
+  } else if (number(arg)) {
+    return new Date(arg).toString() !== 'Invalid Date';
+  }
+  return arg instanceof Date;
+}
 
 /**
  * Ensure a given argument is a number using `typeof`.
  */
-export const number: Validator = (arg) => typeof arg === 'number';
+export function number(arg: unknown): arg is number {
+  return typeof arg === 'number';
+}
 
 /**
  * Ensure a given argument is an object using `typeof`,
@@ -37,19 +53,72 @@ export const number: Validator = (arg) => typeof arg === 'number';
  * Note:
  * Arrays are not considered objects.
  */
-export const object: Validator = (arg) =>
-  typeof arg === 'object' &&
-  !Array.isArray(arg) &&
-  arg !== null &&
-  typeof arg !== 'function';
+export function object(arg: unknown): arg is Record<string, unknown> {
+  return (
+    typeof arg === 'object' &&
+    !Array.isArray(arg) &&
+    arg !== null &&
+    typeof arg !== 'function'
+  );
+}
+
+/**
+ * Ensure a given argument is a string using `typeof`.
+ */
+export function string(arg: unknown): arg is string {
+  return typeof arg === 'string';
+}
+
+/**
+ * Ensure a given argument is a string matching a given pattern.
+ * @param pattern - A regular expression to test against.
+ */
+export function pattern(pattern: RegExp) {
+  return (arg: unknown): arg is string => string(arg) && pattern.test(arg);
+}
+
+/**
+ * Ensure a given argument is `null` or `undefined`.
+ * This is useful for optional fields.
+ */
+export function nullish(arg: unknown): arg is null | undefined {
+  return arg === null || arg === undefined;
+}
+
+/**
+ * Combine multiple validators into a single result.
+ * If any validator fails, the result is false.
+ *
+ * Note: Validators are evaluated in order and only until
+ * the first failure.
+ */
+export const combine: ValidatorOp =
+  (...validators) =>
+  (arg) => {
+    let index = 0;
+
+    while (index < validators.length) {
+      const validator = validators[index];
+
+      if (!validator(arg)) {
+        return false;
+      }
+
+      index++;
+    }
+
+    return true;
+  };
 
 /**
  * Mark a field as optional, only validating if it is present.
  */
 export const optional: ValidatorOp = (validatorFn) => (arg) =>
-  arg ? validatorFn(arg) : true;
+  nullish(arg) ? true : validatorFn(arg);
 
-/**
- * Ensure a given argument is a string using `typeof`.
- */
-export const string: Validator = (arg) => typeof arg === 'string';
+type Validator = (arg: unknown) => boolean;
+type TypedValidator<T = unknown> = (arg: unknown) => arg is T;
+
+type ValidatorOp = (
+  ...validatorFn: (Validator | TypedValidator)[]
+) => Validator;
